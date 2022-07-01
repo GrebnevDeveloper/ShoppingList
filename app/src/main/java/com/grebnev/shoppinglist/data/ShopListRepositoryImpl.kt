1d28cd76180e00e4e053176c13ae9d68de621027
grebnev <grebnev.developer@gmail.com>
2022-07-01 15:33:50 +0400

    Add recycler view for shop list

--- app/src/main/java/com/grebnev/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/grebnev/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -4,17 +4,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.grebnev.shoppinglist.domain.ShopItem
 import com.grebnev.shoppinglist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
 
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
     private var shopItemIdIncrement = 0
 
     init {
-        for (i in 0 until 10) {
-            val shopItem = ShopItem("Name $i", i, true)
+        for (i in 0 until 1000) {
+            val shopItem = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(shopItem)
         }
     }
--- app/src/main/java/com/grebnev/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/grebnev/shoppinglist/presentation/MainActivity.kt
@@ -1,24 +1,80 @@
 package com.grebnev.shoppinglist.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.grebnev.shoppinglist.R
-import com.grebnev.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: ShopListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-        viewModel.deleteShopItem(viewModel.shopList.value!![0])
-        viewModel.changeEnabledState(viewModel.shopList.value!![1])
         viewModel.shopList.observe(this) {
-            Log.d("MainViewModel", it.toString())
+            adapter.submitList(it)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
+        with(rvShopList) {
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_ENABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_DISABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+        }
+
+        setupLongClickListener()
+        setupClickListener()
+        setupSwipeListener(rvShopList)
+    }
+
+    private fun setupSwipeListener(rvShopList: RecyclerView) {
+        val callback = object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = adapter.currentList[viewHolder.adapterPosition]
+                viewModel.deleteShopItem(item)
+            }
+
+        }
+        ItemTouchHelper(callback).attachToRecyclerView(rvShopList)
+    }
+
+    private fun setupClickListener() {
+        adapter.onShopItemClickListener = {
+            Log.d("OnShopIteshopList = itmClickListener", it.toString())
+        }
+    }
+
+    private fun setupLongClickListener() {
+        adapter.onShopItemLongClickListener = {
+            viewModel.changeEnabledState(it)
         }
     }
 }
